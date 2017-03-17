527e7972ea4393ea4e189a6c583dc7dc9ff50f4e
ramon <ramon.mecatec@gmail.com>
2017-03-16 22:52:26 -0300

    [Update]Added speed control and comments in the code
    Since iTween move use time_travel rather than speed. I had to put an
    logic to take the inverse of the speed (which is the time) value and
    send to this function.

--- Assets/VR-Tools/Scripts/waypointLogic.cs
+++ Assets/VR-Tools/Scripts/waypointLogic.cs
@@ -4,35 +4,29 @@ using UnityEngine;
 
 public class waypointLogic : MonoBehaviour {
 
-	public GameObject player;
+	public GameObject player;				// Here is our player/main camera
+	public float player_height = 2;			// The distance between camera and ground. This allow us to maintain the height when traveling through waypoints with differents heights
 
-	public float height = 2;
-	public bool teleport = true;
-
-	public float maxMoveDistance = 10;
-	private bool moving = false;
-
-	// Use this for initialization
-	void Start () {
-
-	}
-
-	// Update is called once per frame
-	void Update () {
-
-	}
+	public bool teleport_move = true;		// Activate teleport between waypoints. When not enabled the user will travel with a linear velocity
+	public float maxMoveDistance = 10;		// Max distance allowed to travel
+	//public float time_to_travel = .8f;		// The time spent by traveling between the waypoints. It's the inverse of the velocity, the less the faster
+	public float speed = .8f;
 
 	public void Move(GameObject waypoint) {
-		if (!teleport) {
+		if (!teleport_move) {
+
+			// Time is the inversal of velocity. Using the same distance, more speed means less time to travel
+			float time_to_travel = Mathf.Abs(speed - 1f);
+
 			iTween.MoveTo (player, 
 				iTween.Hash (
-					"position", new Vector3 (waypoint.GetComponent<Transform> ().position.x, waypoint.GetComponent<Transform> ().position.y + height / 2, waypoint.GetComponent<Transform> ().position.z), 
-					"time", .2F, 
+					"position", new Vector3 (waypoint.GetComponent<Transform> ().position.x, waypoint.GetComponent<Transform> ().position.y + player_height / 2, waypoint.GetComponent<Transform> ().position.z), 
+					"time", speed, 
 					"easetype", "linear"
 				)
 			);
 		} else {
-			player.transform.position = new Vector3 (waypoint.GetComponent<Transform> ().position.x, waypoint.GetComponent<Transform> ().position.y + height / 2, waypoint.GetComponent<Transform> ().position.z);
+			player.transform.position = new Vector3 (waypoint.GetComponent<Transform> ().position.x, waypoint.GetComponent<Transform> ().position.y + player_height / 2, waypoint.GetComponent<Transform> ().position.z);
 		}
 	}
 }
