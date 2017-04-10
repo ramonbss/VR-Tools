4e3dbe13af7f46fe912362e8be98adff8e8b4c6c
Ramon Brito <ramon.mecatec@gmail.com>
2017-04-10 17:47:59 -0300

    feat Add click teleport code

--- Assets/VR Essentials/Scripts/VR_Moviments.cs
+++ Assets/VR Essentials/Scripts/VR_Moviments.cs
@@ -4,21 +4,23 @@ using UnityEngine;
 
 public class VR_Moviments : MonoBehaviour {
 
-	public Transform Player;
-	public void sensitivity = .5f;
+	public Transform MainCamera;
+	public float sensitivity = .5f;
 
 	public bool click_teleport = false;
 	public bool auto_walk = false;
 
 	// Use this for initialization
 	void Start () {
-		
+		MainCamera = GameObject.Find ("Main Camera").transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetMouseButton(0)) {      //This works with the cardboard trigger too.
-			Player.Translate(Vector3.forward * sensitivity);
+
+		if ( click_teleport && Input.GetMouseButton(0)) {      //This works with the cardboard trigger too.
+			transform.Translate( MainCamera.forward * sensitivity);
 		}
+
 	}
 }
