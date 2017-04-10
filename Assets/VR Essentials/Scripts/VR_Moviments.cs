e85fbf6d7a6da41b3863c2799d3a2caa837dd969
Ramon Brito <ramon.mecatec@gmail.com>
2017-04-10 17:18:39 -0300

    added Add script for motion
    Add script for differents type of motion in VR. Like auto walk and click
    teleport.

---
+++ Assets/VR Essentials/Scripts/VR_Moviments.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VR_Moviments : MonoBehaviour {
+
+	public Transform Player;
+	public void sensitivity = .5f;
+
+	public bool click_teleport = false;
+	public bool auto_walk = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton(0)) {      //This works with the cardboard trigger too.
+			Player.Translate(Vector3.forward * sensitivity);
+		}
+	}
+}
